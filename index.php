8d467b3f168605d39ee6c01da6c3d55961e74fe9
ocagnen <ocagnen2@gmail.com>
2020-05-01 10:15:48 +0200

    Comienza cabecera

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilos.css">
+    <title>Proyecto mascotas</title>
+</head>
+<body>
+    <header>
+        <div>
+            <img src="img/logo.svg" alt="logo_empresa">
+            <h1 id='titular_web'>PROYECTO</h1>
+        </div>
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="">Inicio</a>
+                    </li>
+                    <li>
+                        <a href="">Términos y condiciones</a>
+                    </li>
+                    <li>
+                        <a href="">Contacto</a>
+                    </li>
+                    <li>
+                        <a href="">Iniciar sesión</a> 
+                    </li>
+                    <li>
+                        <a href="">Registrarse</a> 
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        <div>
+            <a href=""><img width="90px" src="img/usuarios/usuario1.jpg" alt="Usuario" id="logo_usuario"></a>
+        </div>
+    </header>
+</body>
+</html>
