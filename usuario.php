0755bca3d8942c725cb144f199e49594c7285c7f
ocagnen <ocagnen2@gmail.com>
2020-05-03 15:04:03 +0200

    Crea la estructura de la página de usuario y estilos

---
+++ usuario.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/terminos.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet"> 
+    <script src="jq/jquery-3.1.1.min.js" type="text/javascript"></script>	
+    <script src="js/script.js"></script>
+    <title>Proyecto mascotas</title>
+</head>
+<body>
+    <header>
+        <div id='nombre_empresa'>
+            <h1 id='titular_web'>Au Pet</h1>    
+        </div>
+        <div id='wrapper'>
+            <img id='bars' src="img/bars_green.svg" alt="">
+        </div>
+        <div id='cabecera_lista'>            
+            <nav id='menu_cabecera'>
+                <div>
+                    <a href="index.php">Inicio</a>
+                </div>
+                <div>
+                    <a href="terminos.php">Términos y condiciones</a>
+                </div>
+                <div>
+                    <a href="contacto.php">Contacto</a>
+                </div>
+                <div>
+                    <a href="registro.php">Iniciar Sesión</a>
+                </div>
+                <div>
+                    <a href="registro.php">Registrarse</a>
+                </div>
+            </nav>
+        </div>
+        <div id='contenedor_foto_perfil'>
+            <a href=""><img src="img/usuarios/usuario2.jpg" alt="Usuario" id="logo_usuario"></a>
+        </div>        
+    </header>
+    
+    <footer>
+        <div id='logo_footer'>
+            <h3 id='nombre_footer'>Au Pet</h3>
+        </div>
+        <div id='direcciones_footer'>
+        <div  class='enlace_footer'>
+                <a href="registro.php#reg">¿Dónde me registro?</a>
+            </div>
+            <div class='enlace_footer'>
+                <a href="registro.php#ini">¿Cómo inicio sesión?</a>
+            </div>
+            <div class='enlace_footer'>
+                <a href="contacto.php#correo">Contacto</a>
+            </div>
+            <div class='enlace_footer'>
+                <a href="terminos.php">Términos y condiciones</a>
+            </div>
+            <div class='enlace_footer'>
+                <a href="contacto.php#container_preguntas">Preguntas frecuentes</a>
+            </div>
+        </div>
+        <div id='redes_sociales_footer'>
+            <div>
+                <a href="">
+                    <img src="img/socialMedia/facebook.svg" alt="">
+                </a>
+            </div>
+            <div>
+                <a href="">
+                    <img src="img/socialMedia/insta.svg" alt="">
+                </a>
+            </div>
+        </div>
+        <div id='container_derechos'>
+            <p>©2020 Au Pet. Todos los derechos reservados.</p>
+        </div>
+    </footer>
+</body>
+</html>
